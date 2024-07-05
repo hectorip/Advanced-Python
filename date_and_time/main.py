3230fb4a291102b7f2f17278eddd90c93cc9e061
Héctor Iván Patricio Moreno <hectorivanpatriciomoreno@gmail.com>
2024-07-04 23:52:38 -0600

    WIP

--- date_and_time/main.py
+++ date_and_time/main.py
@@ -4,4 +4,8 @@ from datetime import datetime
 #
 # Para manejar fechas y horas en Python, se utiliza el módulo datetime.
 #
-#
+# El módulo datetime proporciona clases para manejar fechas y horas.
+# 
+# datetime.now() devuelve la fecha y hora actuales.
+
+
